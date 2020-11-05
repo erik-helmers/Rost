9a52cfab754cf801bdeb3d111cfbc8c98e431d0a
Erik Helmers <erik.helmers@outlook.fr>
2020-11-05 21:57:48 +0100

    Added instructions and port utils in prevision of serialprint.

---
+++ src/arch/x86_64/instructions.rs
@@ -0,0 +1,69 @@
+
+
+// Here we implement i/o instruction, namely in(x) / out(x) commands and sti, cli
+// With the 3 possible sizes, byte, word, double word
+// See:
+// https://doc.rust-lang.org/beta/unstable-book/library-features/llvm-asm.html
+// https://doc.rust-lang.org/beta/unstable-book/library-features/asm.html
+// https://hjlebbink.github.io/x86doc/html/IN.html
+// https://hjlebbink.github.io/x86doc/html/OUT.html
+
+pub unsafe fn sti(){
+    asm!("sti",  options(nostack, nomem));
+}
+
+pub unsafe fn cli(){
+    asm!("cli", options(nostack, nomem));
+}
+
+    
+
+
+
+/// Write a byte to specified port
+#[inline(always)]
+pub unsafe fn outb(port:u16, data: u8){
+    asm!( "out dx, al", in("dx") port, in("al") data);
+}
+
+
+/// Reads a byte from specified port
+#[inline(always)]
+pub unsafe fn inb(port:u16) -> u8 {
+    let ret :u8;
+    asm!( "in al, dx", in("dx") port, lateout("al") ret);
+    ret
+}
+
+// 16 bit 
+
+#[inline(always)]
+/// Writes a word to specified port
+pub unsafe fn outw(port: u16, data: u16) {
+    asm!( "out dx, ax", in("dx") port, in("ax") data);
+}
+
+#[inline(always)]
+/// Reads a word from specified port
+pub unsafe fn inw(port:u16) -> u16 {
+    let ret: u16;
+    asm!( "in ax, dx", in("dx") port, lateout("ax") ret);
+    ret
+}
+
+// 32 bit 
+
+#[inline(always)]
+/// Writes a double word to specified port
+pub unsafe fn outdw(port: u16, data: u32) {
+    asm!( "out dx, eax", in("dx") port, in("eax") data);
+}
+
+#[inline(always)]
+/// Reads a double word from specified port
+pub unsafe fn indw(port:u16) -> u32 {
+    let ret: u32;
+    asm!( "in eax, dx", in("dx") port, lateout("eax") ret);
+    ret
+}
+
---
+++ src/utils/port.rs
@@ -0,0 +1,58 @@
+
+use core::marker::PhantomData;
+
+pub struct Port<T: PortSize> {
+    num: u16,
+    _psize: PhantomData<T>
+}
+
+pub trait PortSize {
+    unsafe fn in_(port: u16) -> Self;
+    unsafe fn out(port: u16, val: Self);
+}
+
+
+use crate::arch::instructions as instrux;
+
+impl<T:PortSize> Port<T> {
+    // Create a port with the specified number
+    pub fn new(port: u16) -> Port<T> {
+        Port{num:port, _psize: PhantomData}
+    }
+
+    /// Reads a value from port
+    /// Safety:
+    /// This function may or may not have side effects.
+    /// Depending on the port written to.
+    #[inline(always)]
+    pub fn read(&self) -> T {
+        unsafe{T::in_(self.num)}
+    }
+
+    /// Reads a value from port
+    /// Safety:
+    /// The output may or may not be garbage.
+    /// But calling this function won't corrupt memory
+    #[inline(always)]
+    pub fn write(&self, val: T) {
+        unsafe{T::out(self.num, val)}
+    }
+    
+}
+
+
+// This is just a bind for each type of in_/out to the appropriate asm instructions
+impl PortSize for u8 {
+    #[inline(always)] unsafe fn in_(port: u16) -> Self    { instrux::inb(port) }
+    #[inline(always)] unsafe fn out(port: u16, val: Self) { instrux::outb(port, val) }
+}
+
+impl PortSize for u16 {
+    #[inline(always)] unsafe fn in_(port: u16) -> Self { instrux::inw(port) }
+    #[inline(always)] unsafe fn out(port: u16, val: Self) { instrux::outw(port, val) }
+}
+
+impl PortSize for u32 {
+    #[inline(always)] unsafe fn in_(port: u16) -> Self { instrux::indw(port) }
+    #[inline(always)] unsafe fn out(port: u16, val: Self) { instrux::outdw(port, val) }
+}
