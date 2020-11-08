47519e1aecefa8800d0fa285c4f544b941d22d17
Erik Helmers <erik.helmers@outlook.fr>
2020-11-08 13:37:15 +0100

    Added double fault handler and test for IDT setup.

--- src/arch/x86_64/interrupts.rs
+++ src/arch/x86_64/interrupts.rs
@@ -6,17 +6,23 @@ use super::idt::{Frame, IDT, GateDescriptorType, Entry};
 
 pub fn init_idt() {
     unsafe {
-
-        IDT.breakpoint_excpt.set_handler(breakpoint_handler);
+        
+        IDT.breakpoint_excpt.set_handler(breakpoint);
+        IDT.double_fault_excpt.set_handler(double_fault);
         IDT.load();
     }
 
 }
 
 
-pub extern "x86-interrupt" fn breakpoint_handler(
+pub extern "x86-interrupt" fn breakpoint(
     stack_frame: &mut Frame)
 {
     serial_println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
+pub extern "x86-interrupt" fn double_fault(
+    stack_frame: &mut Frame, _error_code: u64) -> !
+{
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
---
+++ tests/interrupts.rs
@@ -0,0 +1,56 @@
+//! This is a test to check that IDT loading 
+//! works correctly. It just loads a breakpoint 
+//! handler in the IDT and exits successfully if 
+//! said handler is called.
+//! 
+//! In case of a triple fault, the exit code is not 50
+//! and thus will be interpreted as a fail.
+
+#![no_std]
+#![no_main]
+
+
+#![feature(custom_test_frameworks)]
+#![test_runner(rost_nbs::test::runner)]
+#![reexport_test_harness_main = "test_main"]
+
+
+#![feature(abi_x86_interrupt)]
+#![feature(asm)]
+#![allow(unused_unsafe)]
+use rost_nbs::*;
+use rost_nbs::arch::idt::{Frame, IDT};
+import_commons!();
+
+
+#[no_mangle]
+pub unsafe extern "sysv64" fn _start(_boot_info: *const u8) {
+    
+    // Custom interrupt setup
+    unsafe {
+        IDT.breakpoint_excpt.set_handler(breakpoint);
+        IDT.load();
+    }
+
+    serial_print!("Testing that IDT is setup correctly : ");
+    // This should not be mapped
+    unsafe { 
+        asm!("int 3");
+    }
+    panic!("Memory should not have been mapped.");
+}
+
+pub extern "x86-interrupt" fn breakpoint(
+    _stack_frame: &mut Frame) 
+{
+    use devices::qemu_debug::*;
+    serial_println!("[ok]");
+    exit(Status::Success);
+}
+
+
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    test::panic_handler(info);
+}
